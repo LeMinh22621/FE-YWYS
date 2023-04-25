ec4509f5e43de4e3a97b4408e3f57d2cee0782af
lhm2k1 <minh.lehong@digicommercegroup.com>
2023-04-25 17:54:57 +0700

    change some style of navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import styles from '../navbar/style.module.css';
-import * as Unicons from '@iconscout/react-unicons';
+import * as HIIcons from "react-icons/hi";
+import avatar from "../../assets/images/avatar.jpg";
+
 const Navbar = () => {
     return (
         <div className={styles.navbar__container}>
-           <div className={styles.navbar__container_wrapper}>
+            <div className={styles.navbar__container_wrapper}>
                 <div className={styles.home_icon_container} >
-                    <Unicons.UilHome size={30} className={styles.home_icon}/>
+                    <HIIcons.HiHome size={40} className={styles.home_icon} />
                 </div>
-           </div>
+                <div className={styles.wrapper_profile}>
+                        <div className={styles.my_infor}>
+                            <p>Le Minh</p>
+                            <div className={styles.img_container}>
+                                <img
+                                    className={styles.img_avt}
+                                    src={avatar}
+                                    alt='error'
+                                />
+                            </div>
+                        </div>
+                    </div>
+            </div>
         </div>
     );
 }
